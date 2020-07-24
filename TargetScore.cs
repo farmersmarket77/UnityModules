b9ecabe34a38989c19150b0188422d815e4e0b00
FarmersMarket77 <67625787+farmersmarket77@users.noreply.github.com>
2020-07-24 17:57:59 +0900

    target score calculate
    with 3Dcollider

---
+++ TargetScore.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class TargetScore : MonoBehaviour
+{
+    public Transform centerPos;
+    public Transform edgePos;
+
+    private BoxCollider col;
+    private Vector3 inputVec, localVec;
+    private Vector2 finalPos;
+    private float ratio;
+
+    private void Awake()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void CalculateScore()
+    {
+        Vector2 tempVec;
+        float colSize = col.size.x;
+        colSize /= 2;
+        localVec = transform.InverseTransformPoint(inputVec);
+        tempVec = localVec / colSize;
+
+        float tempVecMagitude = tempVec.magnitude;
+        float score = 1 - tempVecMagitude;
+        score = (int)((score + 0.1f) * 10.0f);
+        Debug.Log(score);
+    }
+
+    public void InputProjecetilePos(Vector3 _vec)
+    {
+        inputVec = _vec;
+        CalculateScore();
+    }
+
+    public int ReturnScore()
+    {
+        int score = 0;
+
+        return score;
+    }
+
+    public void DebugTargetScore()
+    {
+        CalculateScore();
+    }
+}
